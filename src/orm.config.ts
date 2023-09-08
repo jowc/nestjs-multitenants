bba6036e66b7420865efc0808c313d14efaf6237
Joseph Chikeme <jchikeme@gmail.com>
2023-09-08 14:41:58 +0100

    installed dependencies and configured database settings

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -3,15 +3,15 @@ import { SnakeNamingStrategy } from './snake-naming.strategy';
 import { join } from 'path';
 
 module.exports = {
-  type: 'postgres',
-  host: 'localhost',
+  type: "postgres",
+  host: "localhost",
   port: 5432,
-  username: 'thomasvanderstraeten',
-  password: 'root',
-  database: 'nestjs-multi-tenant',
+  username: "postgres",
+  password: "root",
+  database: "nestjs-multi-tenant",
   namingStrategy: new SnakeNamingStrategy(),
   logging: true,
   autoLoadEntities: true,
-  entities: [join(__dirname, './modules/public/**/*.entity{.ts,.js}')],
-  migrations: [join(__dirname, './migrations/public/*{.ts,.js}')],
-};
+  entities: [join(__dirname, "./modules/public/**/*.entity{.ts,.js}")],
+  migrations: [join(__dirname, "./migrations/public/*{.ts,.js}")],
+}
